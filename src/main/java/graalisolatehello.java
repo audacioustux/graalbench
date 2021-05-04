47854f6e4c5364c60b142db90ed422b0cfbdfe01
Tanjim Hossain <audacioustux@protonmail.com>
2021-05-04 13:57:45 +0600

    spawnandtear

--- src/main/java/graalisolatehello.java
+++ src/main/java/graalisolatehello.java
@@ -1,7 +1,8 @@
-import org.graalvm.nativeimage.IsolateThread;
-import org.graalvm.nativeimage.Isolates;
+import org.graalvm.nativeimage.*;
 import org.graalvm.nativeimage.c.constant.CEnum;
 import org.graalvm.nativeimage.c.function.CEntryPoint;
+import org.graalvm.nativeimage.c.type.CCharPointer;
+import org.graalvm.nativeimage.c.type.CTypeConversion;
 
 class PerformanceTest {
     //Declaration of initial timestamps based on time unit
@@ -28,14 +29,45 @@ class PerformanceTest {
 
 public class graalisolatehello {
     public static void main(String[] args) {
-        var isolateCount = (args.length != 0) ? Integer.parseInt(args[0]) : 1;
+        final var isolateCount = (args.length != 0) ? Integer.parseInt(args[0]) : 1;
+
+        IsolateThread mainCtx = CurrentIsolate.getCurrentThread();
 
         PerformanceTest timer = new PerformanceTest();
         timer.startNanoTimestamp();
         for (int i = 0; i < isolateCount; i++) {
             var isolateCtx = Isolates.createIsolate(Isolates.CreateIsolateParameters.getDefault());
+            ObjectHandle greetHandle = copyString(isolateCtx, "hello");
+
             Isolates.tearDownIsolate(isolateCtx);
         }
         System.out.println(timer.stopNanoTimestamp());
+    }
+
+    private static ObjectHandle copyString(IsolateThread targetContext, String sourceString) {
+        try (CTypeConversion.CCharPointerHolder cStringHolder = CTypeConversion.toCString(sourceString)) {
+            return copyString(targetContext, cStringHolder.get());
+        }
+    }
+
+    @CEntryPoint
+    private static ObjectHandle copyString(@CEntryPoint.IsolateThreadContext IsolateThread renderingContext, CCharPointer cString) {
+        String targetString = CTypeConversion.toJavaString(cString);
+        return ObjectHandles.getGlobal().create(targetString);
+    }
+
+    @CEntryPoint
+    private static ObjectHandle plotAsSVG(@CEntryPoint.IsolateThreadContext IsolateThread isolateCtx, IsolateThread mainCtx, ObjectHandle greetHandle) {
+        String greetStr = ObjectHandles.getGlobal().get(greetHandle);
+        ObjectHandles.getGlobal().destroy(greetHandle);
+
+        String greetingMsg = String.join(" ", greetStr, "isolate");
+
+        ObjectHandle byteBufferHandle;
+        try (PinnedObject pin = PinnedObject.create(greetingMsg)) {
+            byteBufferHandle = createByteBuffer(isolateCtx, pin.addressOfArrayElement(0), greetingMsg.length());
+        }
+
+        return byteBufferHandle;
     }
 }
